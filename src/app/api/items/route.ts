de1c0d773823204ea881ee88744f1a08271f809e
John Polacek <johnpolacek@veg.vet>
2023-10-31 05:50:58 -0500

    refactor

--- src/app/api/items/route.ts
+++ src/app/api/items/route.ts
@@ -1,14 +1,18 @@
+export const revalidate = 0
+
 import { DynamoDBClient, ScanCommand } from "@aws-sdk/client-dynamodb";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
 const client = new DynamoDBClient({});
 
-export async function GET(req: NextRequest): Promise<void | NextResponse> {
+export async function GET(req: Request): Promise<void | NextResponse> {
   try {
     const { Items } = await client.send(
-      new ScanCommand({ TableName: process.env.TABLE_NAME })
+      new ScanCommand({ TableName: process.env.TABLE_NAME, ConsistentRead: true }),
     );
-    return NextResponse.json(Items, { status: 200 });
+    const response = NextResponse.json(Items, { status: 200 });
+    response.headers.set('Cache-Control', 's-maxage=0, stale-while-revalidate');
+    return response;
   } catch (error) {
     console.error(error);
     return NextResponse.json(
--- src/app/components/items-list.js
+++ src/app/components/items-list.js
@@ -1,76 +1,94 @@
 "use client";
-import useSWR, { mutate } from "swr";
-import { useState } from "react";
-import fetcher from "./util/fetcher";
+import { useState, useEffect } from "react";
 
 export default function ItemsList() {
-  const { data, error } = useSWR("/api/items", fetcher);
+  const [items, setItems] = useState([]);
   const [newItemContent, setNewItemContent] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  const handleAddItem = async () => {
-    const newItem = { id: { S: "" }, content: { S: newItemContent } }; // Temp ID will be replaced by actual ID from server
-    const optimisticData = data ? [...data, newItem] : [newItem];
+  const fetchItems = async () => {
+    try {
+      const response = await fetch("/api/items", {
+        headers: {
+          'Cache-Control': 'no-cache'
+        }
+      });
+      if (!response.ok) {
+        throw new Error(`Error: ${response.statusText}`);
+      }
+      const data = await response.json();
+      setItems(data);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-    // Optimistically update the list
-    mutate("/api/items", optimisticData, false);
+  useEffect(() => {
+    fetchItems();
+  }, []);
 
-    const response = await fetch("/api/item/create", {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ content: newItemContent }),
-    });
+  const handleAddItem = async (event) => {
+    event.preventDefault(); // Prevent default form submission
+    // Optimistically update the items list
+    const optimisticUpdate = [...items, { id: { S: "temp-id" }, content: { S: newItemContent } }];
+    setItems(optimisticUpdate);
 
-    if (response.ok) {
-      // If the request is successful, re-fetch /api/items to get the actual updated list
-      mutate("/api/items");
+    try {
+      const response = await fetch("/api/item/create", {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ content: newItemContent }),
+      });
+      if (!response.ok) {
+        throw new Error(`Error: ${response.statusText}`);
+      }
       setNewItemContent(""); // Clear the input field
-    } else {
-      console.error("Failed to add item:", response.statusText);
-      // If the request fails, revert back to the original data
-      mutate("/api/items", data, false);
+    } catch (err) {
+      console.error("Failed to add item:", err.message);
+      // Revert to previous state in case of error
+      fetchItems();
     }
   };
 
   const handleDeleteItem = async (id) => {
-    // Optimistically update the list
-    const updatedData = data.filter((item) => item.id.S !== id);
-    mutate("/api/items", updatedData, false);
+    // Optimistically update the items list
+    const optimisticUpdate = items.filter((item) => item.id.S !== id);
+    setItems(optimisticUpdate);
 
-    const response = await fetch("/api/item/delete", {
-      method: "DELETE",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ id }),
-    });
-
-    if (!response.ok) {
-      console.error("Failed to delete item:", response.statusText);
-      // If the request fails, revert back to the original data
-      mutate("/api/items", data, false);
-    } else {
-      // If the request is successful, re-fetch /api/items to get the actual updated list
-      mutate("/api/items");
+    try {
+      const response = await fetch("/api/item/delete", {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ id }),
+      });
+      if (!response.ok) {
+        throw new Error(`Error: ${response.statusText}`);
+      }
+    } catch (err) {
+      console.error("Failed to delete item:", err.message);
+      // Revert to previous state in case of error
+      fetchItems();
     }
   };
 
-  if (error)
-    return (
-      <div className="text-red-500">An error has occurred: {error.message}</div>
-    );
-  if (!data)
-    return (
-      <div className="text-gray-500 text-xl text-center py-16 italic">
-        Loading...
-      </div>
-    );
+  if (error) {
+    return <div className="text-red-500">An error has occurred: {error.message}</div>;
+  }
+  if (loading) {
+    return <div className="text-gray-500 text-xl text-center py-16 italic">Loading...</div>;
+  }
 
   return (
     <>
       <div className="border mt-2 mb-8">
-        {data.length === 0 ? (
+        {items.length === 0 ? (
           <div className="p-4 text-gray-500">No items yet.</div>
         ) : (
           <ul className="flex flex-col divide-y">
-            {data.map((item, index) => (
+            {items.map((item, index) => (
               <li key={index} className="p-4 flex items-center justify-between">
                 {item.content.S}
                 <button
@@ -86,19 +104,22 @@ export default function ItemsList() {
       </div>
 
       <h3 className="text-lg my-1">Add Item</h3>
-      <input
-        type="text"
-        value={newItemContent}
-        onChange={(e) => setNewItemContent(e.target.value)}
-        placeholder="New item content"
-        className="border border-gray-300 p-2 rounded-lg mb-4 w-full"
-      />
-      <button
-        onClick={handleAddItem}
-        className="bg-blue-500 text-white py-2 px-4 rounded mb-4"
-      >
-        Add Item
-      </button>
+      <form onSubmit={handleAddItem}>
+        <input
+          type="text"
+          value={newItemContent}
+          onChange={(e) => setNewItemContent(e.target.value)}
+          placeholder="New item content"
+          required
+          className="border border-gray-300 p-2 rounded mb-4 w-full"
+        />
+        <button
+          type="submit"
+          className="bg-blue-500 text-white py-2 px-4 rounded-lg mb-4"
+        >
+          Add Item
+        </button>
+      </form>
     </>
   );
 }
